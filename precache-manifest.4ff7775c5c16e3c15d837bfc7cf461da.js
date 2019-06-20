7de26ea24fd1863831e911bf6cd0b6b8024116b8
basantpatidar <basantpatidar33@gmail.com>
2019-06-20 01:16:49 -0400

    Updates

---
+++ precache-manifest.4ff7775c5c16e3c15d837bfc7cf461da.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ee9260a9989a4c595297d95dd600f3",
+    "url": "/Portfolio-Build/index.html"
+  },
+  {
+    "revision": "1e1993ce8d36d3c63180",
+    "url": "/Portfolio-Build/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "32982111c8a123eb1602",
+    "url": "/Portfolio-Build/static/css/main.7142fe44.chunk.css"
+  },
+  {
+    "revision": "1e1993ce8d36d3c63180",
+    "url": "/Portfolio-Build/static/js/2.72222e04.chunk.js"
+  },
+  {
+    "revision": "32982111c8a123eb1602",
+    "url": "/Portfolio-Build/static/js/main.0eff1c00.chunk.js"
+  },
+  {
+    "revision": "826adeb0680ec76322e3",
+    "url": "/Portfolio-Build/static/js/runtime~main.2509f4f6.js"
+  },
+  {
+    "revision": "d2ded310ca37693a9bc4350dce59df35",
+    "url": "/Portfolio-Build/static/media/logo.d2ded310.png"
+  },
+  {
+    "revision": "f94c6fa67ca80b0f74dee84318915845",
+    "url": "/Portfolio-Build/static/media/msSqlProject.f94c6fa6.png"
+  },
+  {
+    "revision": "f7e9212c3c93b0352a39197896fa3575",
+    "url": "/Portfolio-Build/static/media/ngoPortalProject.f7e9212c.PNG"
+  },
+  {
+    "revision": "991aedf61ed0967e6fa52b3d7ff3c024",
+    "url": "/Portfolio-Build/static/media/portfolioProject.991aedf6.PNG"
+  },
+  {
+    "revision": "ad6cd94545ee7bb7fe864d59e5cfebb8",
+    "url": "/Portfolio-Build/static/media/sportsworksProject.ad6cd945.PNG"
+  }
+]);
